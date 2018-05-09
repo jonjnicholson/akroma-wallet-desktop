725010ac063641bc9f7312af867c4a1033fec5aa
Ian DesJardins <ian.d.desjardins@gmail.com>
2018-05-08 22:31:14 -0400

    refactor how we subscribe to observables

--- src/app/components/splash/splash.component.ts
+++ src/app/components/splash/splash.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Observable } from 'rxjs';
 import { distinctUntilChanged } from 'rxjs/operators';
@@ -18,21 +19,55 @@ export function getProgressbarConfig(): ProgressbarConfig {
   providers: [{ provide: ProgressbarConfig, useFactory: getProgressbarConfig }]
 })
 export class SplashComponent implements OnInit {
-  isSyncing$: Observable<boolean>;
-  isListening$: Observable<any>;
-  peerCount$: Observable<number>;
-  constructor(private web3: Web3Service) {
-    console.log(window);
+  lastPercentageSynced: number;
+  isSyncing: boolean | any;
+  isListening: boolean;
+  peerCount: number;
+
+  constructor(private web3: Web3Service,
+              private router: Router) {
     this.web3.setProvider(new this.web3.providers.HttpProvider('http://localhost:8545'));
+    this.lastPercentageSynced = 0;
   }
 
   ngOnInit() {
-    this.isListening$ = Observable.interval(10000)
-    .flatMap((i) => Observable.fromPromise(this.web3.eth.net.isListening())).pipe(distinctUntilChanged());
-    this.isSyncing$ = Observable.interval(5000)
-    .flatMap((i) => Observable.fromPromise(this.web3.eth.isSyncing())).pipe(distinctUntilChanged());
-    this.peerCount$ = Observable.interval(10000)
-    .flatMap((i) => Observable.fromPromise(this.web3.eth.net.getPeerCount())).pipe(distinctUntilChanged());
+    const isListeningSub$ = Observable.interval(10000)
+    .flatMap((i) => Observable.fromPromise(this.web3.eth.net.isListening()))
+    .pipe(distinctUntilChanged())
+    .subscribe(result => {
+      this.isListening = result;
+    });
+
+    const isSyncingSub$ = Observable.interval(1000)
+    .flatMap((i) => Observable.fromPromise(this.web3.eth.isSyncing()))
+    .pipe(distinctUntilChanged())
+    .subscribe((result: boolean | any) => {
+      if (this.isListening) {
+        this.isSyncing = result;
+        if (!!result) {
+          this.lastPercentageSynced = this.currentPercentage(result.currentBlock, result.highestBlock);
+          console.log(this.lastPercentageSynced.toFixed(0));
+        }
+        if (result === false && (this.lastPercentageSynced || 0).toFixed(0) === '100') {
+          // Nav away here
+          console.log('nav away...');
+          // also unsubscribe like so, although we need to validate the order, which we do first
+          // isListeningSub$.unsubscribe();
+          // isSyncingSub$.unsubscribe();
+          // peerCountSub$.unsubscribe();
+          // this.router.navigate(['/path/to/go/to/here']);
+        }
+      }
+    });
+
+    const peerCountSub$ = Observable.interval(1000)
+    .flatMap((i) => Observable.fromPromise(this.web3.eth.net.getPeerCount()))
+    .pipe(distinctUntilChanged())
+    .subscribe(result => {
+      if (this.isListening) {
+        this.peerCount = result;
+      }
+    });
   }
 
   currentPercentage(currentBlock: string, highestBlock: string): number {
