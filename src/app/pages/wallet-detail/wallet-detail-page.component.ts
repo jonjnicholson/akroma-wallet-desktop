3d4e447dae4912ca7a613dffb5685d70c741a925
Stephen Koller <stephenkoller@gmail.com>
2018-05-09 22:46:43 -0400

    Rename cont. - Missed a spot

--- src/app/pages/wallet-detail/wallet-detail-page.component.ts
+++ src/app/pages/wallet-detail/wallet-detail-page.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-wallet-detail-page',
-  templateUrl: './wallet-detail.component.html',
-  styleUrls: ['./wallet-detail.component.scss']
+  templateUrl: './wallet-detail-page.component.html',
+  styleUrls: ['./wallet-detail-page.component.scss']
 })
 export class WalletDetailPageComponent implements OnInit {
 
