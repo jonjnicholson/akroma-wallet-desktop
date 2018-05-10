025f287e8eb13371ebc050a7573f710300ff0d09
Stephen Koller <stephenkoller@gmail.com>
2018-05-09 22:24:18 -0400

    Remove HomeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,6 @@ import { HomeComponent } from './components/home/home.component';
 import { SplashComponent } from './components/splash/splash.component';
 import { WalletComponent } from './components/wallet/wallet.component';
 import { WalletListComponent } from './components/wallet-list/wallet-list.component';
-import { WalletDetailComponent } from './components/wallet-detail/wallet-detail.component';
 import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
 import { Web3Service } from './providers/web3.service';
 import { TransactionsPersistenceService } from './providers/transactions-persistence.service';
@@ -39,7 +38,6 @@ export function HttpLoaderFactory(http: HttpClient) {
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     WebviewDirective,
     SplashComponent,
     WalletComponent,
